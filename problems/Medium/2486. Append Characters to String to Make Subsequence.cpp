9bb7cac5a787c14ccb219929cc024775246ee5c0
anant0059 <2019uce0059@gmail.com>
2023-02-01 09:27:34 +0530

    Medium

---
+++ problems/Medium/2486. Append Characters to String to Make Subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int appendCharacters(string t, string s) {
+        int n=s.size(),m=t.size(),i=0,j=0;
+        while(i<n && j<m){
+            bool flag=0;
+            while(j<m){
+                if(s[i]==t[j]){
+                    flag=1;
+                    ++j;
+                    break;
+                }
+                ++j;
+            }
+            if(flag==0) return n-i;
+            ++i;
+        }
+        cout<<"nf";
+        return n-i;
+    }
+};
