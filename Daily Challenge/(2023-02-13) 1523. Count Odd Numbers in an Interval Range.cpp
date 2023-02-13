9781fbd27a5add2ce5b7a1becbc8a1aeb9ed21b2
anant0059 <2019uce0059@gmail.com>
2023-02-13 23:38:07 +0530

    Easy

---
+++ Daily Challenge/(2023-02-13) 1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if(low%2 && high%2) return ((high-low+2)/2);
+        if(low%2 || high%2) return ((high-low-1)/2)+1;
+        return ((high-low)/2);
+    }
+};
