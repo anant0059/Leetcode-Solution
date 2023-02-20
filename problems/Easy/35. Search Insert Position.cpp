3c2746e46f432c2ebe9a7ef9215d477cfd183b87
anant0059 <2019uce0059@gmail.com>
2023-02-20 14:17:51 +0530

    Easy

---
+++ problems/Easy/35. Search Insert Position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0, right=nums.size()-1, mid;
+        if(target<nums[0]) return 0;
+        if(target>nums[right]) return right+1;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target) right=mid-1;
+            else left=mid+1;
+        }
+        return left;
+    }
+};
