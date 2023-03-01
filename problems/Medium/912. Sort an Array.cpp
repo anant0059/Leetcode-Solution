387a974bc3c825386d619511a3c86d966014a2b8
anant0059 <2019uce0059@gmail.com>
2023-03-01 08:57:32 +0530

    Medium

---
+++ problems/Medium/912. Sort an Array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int> &nums, int low, int mid, int high){
+        if (low >= high) return;
+        int l = low, r = mid + 1, k = 0, size = high - low + 1;
+        vector<int> sorted(size, 0);
+        while (l <= mid and r <= high)
+            sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
+        while (l <= mid) 
+            sorted[k++] = nums[l++];
+        while (r <= high) 
+            sorted[k++] = nums[r++];
+        for (k = 0; k < size; k++)
+            nums[k + low] = sorted[k];
+    }
+
+    void mergesort(vector<int> &nums, int start, int end){
+        if(start>=end){
+            return;
+        }
+        int mid=(start+end)/2;
+        mergesort(nums, start, mid);
+        mergesort(nums, mid+1, end);
+        merge(nums, start, mid, end);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        mergesort(nums, 0, n-1);
+        return nums;
+    }
+};
