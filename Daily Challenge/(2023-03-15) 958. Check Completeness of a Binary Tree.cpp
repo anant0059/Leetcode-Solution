af891ae62e88542a2c67ce414d05a98ecbc11636
anant0059 <2019uce0059@gmail.com>
2023-03-15 10:09:26 +0530

    Medium

---
+++ Daily Challenge/(2023-03-15) 958. Check Completeness of a Binary Tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode* > q;
+        bool null=false;
+        q.push(root);
+        while(q.size()){
+            int sz=q.size();
+            while(sz--){
+                TreeNode* temp=q.front();
+                q.pop();
+                if(null && temp) return false;
+                if(temp==NULL){
+                    null=true;
+                }
+                else{
+                    q.push(temp->left);
+                    q.push(temp->right);
+                }
+            }
+        }
+        return true;
+    }
+};
