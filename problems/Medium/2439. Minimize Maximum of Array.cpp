aa50040ae871c994da2ade51d25bf719f400fa38
anant0059 <2019uce0059@gmail.com>
2023-04-06 15:47:14 +0530

    Medium

--- problems/Medium/2439. Minimize Maximum of Array.cpp
+++ problems/Medium/2439. Minimize Maximum of Array.cpp
@@ -1,8 +1,11 @@
-    int minimizeArrayValue(vector<int>& A) {
-        long sum = 0, res = 0;
-        for (int i = 0; i < A.size(); ++i) {
-            sum += A[i];
-            res = max(res, (sum + i) / (i + 1));
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        long long int ans=0,s=0;
+        for(int i=0;i<nums.size();i++){
+            s+=nums[i];
+            ans=max(ans,(s+i)/(i+1));
         }
-        return res;
+        return ans;
     }
+};
