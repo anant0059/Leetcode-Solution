5f0b5e6c39f24262eec49404612fd541871517e3
anant0059 <2019uce0059@gmail.com>
2023-04-07 09:45:51 +0530

    Medium

---
+++ Daily Challenge/(2023-04-07) 1020. Number of Enclaves.cpp
@@ -0,0 +1,137 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size(), res = 0;
+        vector< vector < int > > move = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+        for( int i = 0; i < m; ++i ){
+            if( grid[0][i] == 1 )
+            {
+                queue< pair< int, int> > q;
+                q.push( { 0, i } );
+                while( q.size() )
+                {
+                    int ro = q.front().first, col = q.front().second;
+                    q.pop();
+                        if( grid[ro][col] == 1 )
+                        {
+                            for( auto ii : move )
+                            {
+                                if( ro + ii[0] < n && ro + ii[0] >= 0 && col + ii[1] < m && col + ii[1] >=0 && grid[ro + ii[0]][col + ii[1]] == 1 )
+                                {
+                                    q.push( { ro + ii[0], col + ii[1] } );
+                                }
+                            }
+                            grid[ro][col] = 0;
+                            // ++res;
+                        }
+                }
+            }
+            if( grid[n-1][i] == 1 )
+            {
+                queue< pair< int, int> > q;
+                q.push( { n-1, i } );
+                while( q.size() )
+                {
+                    int ro = q.front().first, col = q.front().second;
+                    q.pop();
+                        if( grid[ro][col] == 1 )
+                        {
+                            for( auto ii : move )
+                            {
+                                if( ro + ii[0] < n && ro + ii[0] >= 0 && col + ii[1] < m && col + ii[1] >=0 && grid[ro + ii[0]][col + ii[1]] == 1 )
+                                {
+                                    q.push( { ro + ii[0], col + ii[1] } );
+                                }
+                            }
+                            grid[ro][col] = 0;
+                            // ++res;
+                        }
+                }
+            }
+        }
+
+        for( int i = 0; i < n; ++i ){
+            if( grid[i][0] == 1 )
+            {
+                queue< pair< int, int> > q;
+                q.push( { i, 0 } );
+                while( q.size() )
+                {
+                    int ro = q.front().first, col = q.front().second;
+                    q.pop();
+                        if( grid[ro][col] == 1 )
+                        {
+                            for( auto ii : move )
+                            {
+                                if( ro + ii[0] < n && ro + ii[0] >= 0 && col + ii[1] < m && col + ii[1] >=0 && grid[ro + ii[0]][col + ii[1]] == 1 )
+                                {
+                                    q.push( { ro + ii[0], col + ii[1] } );
+                                }
+                            }
+                            grid[ro][col] = 0;
+                            // ++res;
+                        }
+                }
+            }
+            if( grid[i][m-1] == 1 )
+            {
+                queue< pair< int, int> > q;
+                q.push( { i, m-1 } );
+                while( q.size() )
+                {
+                    int ro = q.front().first, col = q.front().second;
+                    q.pop();
+                        if( grid[ro][col] == 1 )
+                        {
+                            for( auto ii : move )
+                            {
+                                if( ro + ii[0] < n && ro + ii[0] >= 0 && col + ii[1] < m && col + ii[1] >=0 && grid[ro + ii[0]][col + ii[1]] == 1 )
+                                {
+                                    q.push( { ro + ii[0], col + ii[1] } );
+                                }
+                            }
+                            grid[ro][col] = 0;
+                            // ++res;
+                        }
+                }
+            }
+        }
+
+        for( int i = 0; i < n; ++i )
+        {
+            for( int j = 0; j < m; ++j )
+            {
+                if( grid[i][j] == 1 )
+                {
+                    queue< pair< int, int> > q;
+                    q.push( { i, j } );
+                    while( q.size() )
+                    {
+                        int ro = q.front().first, col = q.front().second;q.pop();
+                        if( grid[ro][col] == 1 )
+                        {
+                            for( auto ii : move )
+                            {
+                                if( ro + ii[0] < n && ro + ii[0] >= 0 && col + ii[1] < m && col + ii[1] >=0 && grid[ro + ii[0]][col + ii[1]] == 1 )
+                                {
+                                    q.push( { ro + ii[0], col + ii[1] } );
+                                }
+                            }
+                            grid[ro][col] = 0;
+                            ++res;
+                        }
+
+                        // else
+                        // {
+                        //     q.pop();
+                        // }
+                        // ++res;
+                    }
+                }
+                // cout<< grid[i][j]<<" ";
+            }
+            // cout<< endl;
+        }
+        return res;
+    }
+};
