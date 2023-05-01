b07b512a136060c8eae9e4ae507b4628c4ab417d
anant0059 <2019uce0059@gmail.com>
2023-05-01 17:44:59 +0530

    Hard

---
+++ Daily Challenge/(2023-04-28) 839. Similar String Groups.cpp
@@ -0,0 +1,32 @@
+class disjoint_set {
+    vector<int> v;
+    int sz;
+public:
+    disjoint_set(int n) {
+        makeset(n);
+    }
+
+    void makeset(int n) {
+        v.resize(n);
+        iota(v.begin(), v.end(), 0);
+        sz = n;
+    }
+
+    int find(int i) {
+        if (i != v[i])
+            v[i] = find(v[i]);
+        return v[i];
+    }
+    
+    void join(int i, int j) {
+        int ri = find(i), rj = find(j);
+        if (ri != rj) {
+            v[ri] = rj;
+            sz--;
+        }
+    }
+    
+    int size() {
+        return sz;
+    }
+};
