8c6f557bd299c60d359b53880f950e29004a2b93
anant0059 <2019uce0059@gmail.com>
2023-05-29 17:42:59 +0530

    Easy

---
+++ Daily Challenge/(2023-05-29) 1603. Design Parking System.cpp
@@ -0,0 +1,42 @@
+class ParkingSystem {
+    int big;
+    int medium;
+    int small;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        this -> big = big;
+        this -> medium = medium;
+        this -> small = small;
+    }
+    
+    bool addCar(int carType) {
+        if(carType == 1) {
+            if(big > 0) {
+                --big;
+                return true;
+            }
+            return false;
+        }
+        else if(carType == 2) {
+            if(medium > 0) {
+                --medium;
+                return true;
+            }
+            return false;
+        }
+        else {
+            if(small > 0) {
+                --small;
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
