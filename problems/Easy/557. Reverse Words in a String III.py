b694d5749e77db467ce7388624b9bf4a36f75028
anant0059 <2019uce0059@gmail.com>
2023-10-01 18:29:26 +0530

    Easy

---
+++ problems/Easy/557. Reverse Words in a String III.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip().split()                   #Converting s into a list to get rid of spaces
+        out = []
+        for word in s:                          #Reversing each word of the list using two-pointers
+            i = 0
+            j = (len(word) - 1)
+            while (i < j):
+                word = list(word)
+                word[i], word[j] = word[j], word[i]
+                i += 1
+                j -= 1
+            a = (''.join(word))
+            out.append(a)
+        return(' '.join(out))                     #joining the words back to form a string
