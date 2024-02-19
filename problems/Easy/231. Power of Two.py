7539b19672e78370c8a76f4120eaf57d441dfcf1
Anant <2019uce0059@gmail.com>
2024-02-19 22:43:51 +0530

    Easy

--- problems/Easy/231. Power of Two.py
+++ problems/Easy/231. Power of Two.py
@@ -1,17 +1,18 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
+        if n <= 0:
             return 0
-        neg = 0
-        if n < 0:
-            neg = 1
+        # neg = 0
+        # if n < 0:
+        #     neg = 1
         n = abs(n)
-        x = 1
+        x = 0
         while n > 1:
             if n%2:
                 return 0
             x = x + 1
             n = n/2
-        if neg and x%2:
-            return 0
+        # print(neg, x)
+        # if neg and x%2:
+        #     return 0
         return 1
