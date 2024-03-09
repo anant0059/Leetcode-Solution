f537ed42098fa9dfcf22bdb7f327ac7a92b10dc5
Anant <2019uce0059@gmail.com>
2024-03-09 14:58:09 +0530

    Easy

---
+++ problems/Easy/2540. Minimum Common Value.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i = 0
+        j = 0
+        n = len(nums1)
+        m = len(nums2)
+        while i<n and j<m:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i = i + 1
+            else:
+                j = j + 1
+        return -1
