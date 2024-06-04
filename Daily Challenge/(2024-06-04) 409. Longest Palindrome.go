140963bf53ad98260d2e8ce9d769b5345ff8a1e2
anant0059 <2019uce0059@gmail.com>
2024-06-04 20:47:39 +0530

    Easy

---
+++ Daily Challenge/(2024-06-04) 409. Longest Palindrome.go
@@ -0,0 +1,50 @@
+func longestPalindrome(s string) int {
+    freq := make(map[rune]int)
+    for _, c := range s {
+        freq[c]++
+    }
+    res := 0
+    odd := 0
+    for _, count := range freq {
+        if count % 2 == 0 {
+            res += count
+        } else {
+            odd = max(odd, count)
+        }
+    }
+    res += odd
+    return res
+}
+
+
+    // n := len(s)
+    // res := 0
+    // for i:=0 ; i<n ; i++ {
+    //     left := i - 1
+    //     right := i + 1
+    //     tmp := 1
+    //     for left >= 0 && right < n {
+    //         if s[left] == s[right] {
+    //             tmp += 2
+    //         } else {
+    //             break
+    //         }
+    //         left--
+    //         right++
+    //     }
+    //     res = max(res, tmp)
+    //     left = i
+    //     right = i + 1
+    //     tmp = 0
+    //     for left >= 0 && right < n {
+    //         if s[left] == s[right] {
+    //             tmp += 2
+    //         } else {
+    //             break
+    //         }
+    //         left--
+    //         right++
+    //     }
+    //     res = max(res, tmp)
+    // }
+    // return res
