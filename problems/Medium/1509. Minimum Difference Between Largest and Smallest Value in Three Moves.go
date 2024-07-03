f1ad04f8df3ac031a78914d3060e1d3f3838b7ad
anant0059 <2019uce0059@gmail.com>
2024-07-03 13:48:16 +0530

    Medium

--- problems/Medium/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.go
+++ problems/Medium/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.go
@@ -4,18 +4,15 @@ func minDifference(nums []int) int {
 		return 0
 	}
 
-	// Partially sort the first four elements
-	sort.Slice(nums[:4], func(i, j int) bool { return nums[i] < nums[j] })
-	// Find the 4th largest element
-	sort.Slice(nums[4:], func(i, j int) bool { return nums[4+i] < nums[4+j] })
-	// Sort the last four elements
-	sort.Slice(nums[numsSize-4:], func(i, j int) bool { return nums[numsSize-4+i] < nums[numsSize-4+j] })
+	// Sort the array
+	sort.Ints(nums)
 
 	minDiff := math.MaxInt64
 
 	// Four scenarios to compute the minimum difference
-	for left, right := 0, numsSize-4; left < 4; left, right = left+1, right+1 {
-		if diff := nums[right] - nums[left]; diff < minDiff {
+	for i := 0; i < 4; i++ {
+		diff := nums[numsSize-4+i] - nums[i]
+		if diff < minDiff {
 			minDiff = diff
 		}
 	}
